e9cc4e805aba5a17b29b29bcb5538466d216ffda
Colleen <windrainicefire@gmail.com>
2015-11-12 15:55:05 -0800

    Now it takes you to the GAME PAGE!!!!

--- app/src/main/java/com/cultivatingcows/YourGamesActivity.java
+++ app/src/main/java/com/cultivatingcows/YourGamesActivity.java
@@ -2,6 +2,7 @@ package com.cultivatingcows;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -42,36 +43,34 @@ public class YourGamesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_games);
         ButterKnife.bind(this);
-
-
         User theUser = new User(currentUser, TAG, YourGamesActivity.this, new Runnable() {
             @Override
             public void run() {
                 mUserGamesStrings = User.getUserGames();
                 setThatList(mUserGamesStrings, mArrayAdapter, mGamesList);
-                //makeListClickable(mGamesList);
+                makeListClickable(mGamesList);
                 }
             });
     }//END OF ONCREATE
-
     public void setThatList(List<String> stringList, ArrayAdapter<String> arrayAdapter, ListView listView) {
         arrayAdapter = new ArrayAdapter<String>(
                 YourGamesActivity.this,
                 android.R.layout.simple_list_item_1,
                 stringList);
         listView.setAdapter(arrayAdapter);
-        // makeListClickable(listView);
+        //makeListClickable(listView);
     }
-//    public void makeListClickable(ListView listView) {
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+    public void makeListClickable(ListView listView) {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Intent intent = new Intent(YourGamesActivity.this, GameHomeActivity.class);
+                startActivity(intent);
+//                int three = 4;
+//              Toast.makeText(YourGamesActivity.this, "Waka waka", Toast.LENGTH_LONG);
 //                final String gameName = (String) arg0.getItemAtPosition(position);
-//                Toast.makeText(YourGamesActivity.this, "You are a player in ", Toast.LENGTH_SHORT);
-//
-//            }
-//        });
-//
-//    }
-
+//                Toast.makeText(YourGamesActivity.this, "You are a player in " + gameName, Toast.LENGTH_SHORT);
+            }
+        });
+    }
 }
