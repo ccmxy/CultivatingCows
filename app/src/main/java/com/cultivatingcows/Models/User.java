9c0d1b9c596483c84f35082896dff1a7ef7bde31
ccmxy <windrainicefire@gmail.com>
2015-11-15 13:13:14 -0800

    "You can now get a list of players of a game"

--- app/src/main/java/com/cultivatingcows/Models/User.java
+++ app/src/main/java/com/cultivatingcows/Models/User.java
@@ -3,14 +3,16 @@ package com.cultivatingcows.Models;
 import android.app.Activity;
 
 import com.cultivatingcows.ErrorHelper;
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
-import com.parse.Parse;
 import com.parse.ParseClassName;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
 import java.util.List;
+
 
 /**
  * Created by colleenminor on 11/11/15.
@@ -18,29 +20,29 @@ import java.util.List;
 
 @ParseClassName("_User")
 public class User extends ParseUser {
-
+    private static List<ParseUser> mPlayers;
     private ParseUser mParseUser;
     private String mEmail;
     private String mUsername;
     private String[] mGameNameStringArray;
     private static List<String> mUserGames;
 
-    public User(){
+    public User() {
         super();
     }
 
-    public User(ParseUser thisUser, final String TAG, final Activity context, final Runnable runnable){
+    public User(ParseUser thisUser, final String TAG, final Activity context, final Runnable runnable) {
         mUsername = thisUser.getUsername();
         mEmail = thisUser.getEmail();
         mUserGames = thisUser.getList("game");
         context.runOnUiThread(runnable);
     }
 
-    public static List<String> getUserGames(){
+    public static List<String> getUserGames() {
         return mUserGames;
     }
 
-    public User(String username, String password, String email){
+    public User(String username, String password, String email) {
         setUsername(username);
         mUsername = username;
         setPassword(password);
@@ -48,7 +50,7 @@ public class User extends ParseUser {
         mEmail = email;
     }
 
-    public void register(final String tag, final Activity context, final Runnable runnable){
+    public void register(final String tag, final Activity context, final Runnable runnable) {
         signUpInBackground(new SignUpCallback() {
             @Override
             public void done(ParseException e) {
@@ -64,17 +66,43 @@ public class User extends ParseUser {
             }
         });
     }
+
     public static void logIn(final String username, final String password, final String tag, final Activity context, final Runnable runnable) {
-                  ParseUser.logInInBackground(username, password, new LogInCallback() {
-                        @Override
-                        public void done(ParseUser parseUser, ParseException e) {
-                            if (e != null) {
-                                ErrorHelper.handleError(tag, context, e.getMessage());
-                            } else {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser parseUser, ParseException e) {
+                if (e != null) {
+                    ErrorHelper.handleError(tag, context, e.getMessage());
+                } else {
                     context.runOnUiThread(runnable);
                 }
             }
         });
+    }
+
+    public static void findPlayers(final String gameName, final String tag, final Activity context, final Runnable runnable) {
+        playersListQuery(gameName).findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> players, com.parse.ParseException e) {
+                if (e == null) {
+                    mPlayers = players;
+                    context.runOnUiThread(runnable);
+                } else {
+                    ErrorHelper.handleError(tag, context, e.getMessage());
+                }
+
+
+            }
+        });
+    }
+
+    public static ParseQuery<ParseUser> playersListQuery(String gameName) {
+        ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
+        return userQuery.whereEqualTo("game", gameName);
+    }
+
+    public static List<ParseUser> getPlayers() {
+        return mPlayers;
     }
 
 }
