0a1ffc96332b7c8f5609df740ada2115c4598113
Colleen <windrainicefire@gmail.com>
2015-11-16 12:25:39 -0800

    Now the quick log-in from the GamePage will keep you on that page

--- app/src/main/java/com/cultivatingcows/UI/GamePageActivity.java
+++ app/src/main/java/com/cultivatingcows/UI/GamePageActivity.java
@@ -213,7 +213,9 @@ public class GamePageActivity extends AppCompatActivity {
                                 User.logIn(loginUserName, password, TAG, GamePageActivity.this, new Runnable() {
                                     @Override
                                     public void run() {
-                                        Intent intent = new Intent(mContext, YourGamesActivity.class);
+                                      //  Intent intent = new Intent(mContext, YourGamesActivity.class);
+                                        Intent intent = getIntent();
+
                                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(intent);
