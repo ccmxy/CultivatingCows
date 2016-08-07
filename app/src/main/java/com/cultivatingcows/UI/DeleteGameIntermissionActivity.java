1731659855ed3179f9ad48f698b1c50bcaeb6931
ccmxy <windrainicefire@gmail.com>
2016-08-06 18:07:34 -0700

    Added delete

--- app/src/main/java/com/cultivatingcows/UI/DeleteGameIntermissionActivity.java
+++ app/src/main/java/com/cultivatingcows/UI/DeleteGameIntermissionActivity.java
@@ -2,15 +2,15 @@ package com.cultivatingcows.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.cultivatingcows.Models.Game;
 import com.cultivatingcows.R;
 import com.parse.ParseObject;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DeleteGameIntermissionActivity extends AppCompatActivity {
     private static final String TAG = DeleteGameIntermissionActivity.class.getSimpleName();
@@ -40,16 +40,15 @@ public class DeleteGameIntermissionActivity extends AppCompatActivity {
 
             }
         });
+        Timer timer = new Timer();
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                Intent intent = new Intent(DeleteGameIntermissionActivity.this, MainActivity.class);
+                startActivity(intent);
             }
-        });
+        }, 3000);
+
     }
 
 }
